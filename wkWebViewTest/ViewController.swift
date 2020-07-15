4f6d3b32c19b5494358b9dd676ecaf9032a53ea4
KMY <muziq43@hanafn.com>
2020-07-15 16:45:20 +0900

    decidePolicyFor 관련 추가.

--- wkWebViewTest/ViewController.swift
+++ wkWebViewTest/ViewController.swift
@@ -97,17 +97,50 @@ extension ViewController:WKNavigationDelegate{
     
     /*
      * 웹페이지의 탐색 허용을 결정
+     * WKNavigationAction / URLRequest
+     *
      */
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-
+        print("decidePolicyFor navigationAction() call")
+        print("\(navigationAction.request.url!.absoluteString)")
+        
+        let chkUrl : String = navigationAction.request.url!.absoluteString
+        
+        if chkUrl.hasPrefix("https:")||chkUrl.hasPrefix("http:") {
+            if chkUrl.contains("801sanae") {
+                decisionHandler(.allow)
+            }
+        }else{
+            decisionHandler(.cancel)
+        }
     }
-    
+
+    /*
+     * 웹페이지의 탐색 허용을 결정
+     * WKNavigationResponse / HTTPURLResponse
+     */
     func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-
+        print("decidePolicyFor navigationResponse() call")
+//        if navigationResponse.response is HTTPURLResponse {
+            let resp = navigationResponse.response as? HTTPURLResponse
+            
+        if resp?.statusCode == 500 || resp?.statusCode == 404 {
+            decisionHandler(.cancel)
+        } else if resp?.statusCode == 200 {
+            decisionHandler(.allow)
+        }
     }
-    
+    /*
+     * ContentMode 추가.
+     * .recommand, .desktop, .mobile
+     *  컨텐츠의 유형 및 로드된 결과에 적용되는 레이아웃, 렌더링 조정에 영향을 줌?..
+     *  WKNavigationAction / URLRequest 유사.? 비슷? 같음?
+     */
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, preferences: WKWebpagePreferences, decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
-
+        print("decidePolicyFor navigationAction preferences() call")
+        preferences.preferredContentMode = .recommended
+        
+        decisionHandler(.allow, preferences)
     }
     
     // 웹뷰가 콘텐츠 탐색을 시작할 떄 호출
@@ -144,6 +177,18 @@ extension ViewController:WKNavigationDelegate{
         print("didFail()")
         self.alert("상세 페이지를 읽어오지 못했습니다.")
         print("error Msg ==\(error.localizedDescription)")
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        print("runJavaScriptAlertPanelWithMessage()")
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        print("runJavaScriptConfirmPanelWithMessage()")
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        print("runJavaScriptTextInputPanelWithPrompt()")
     }
     
 }
